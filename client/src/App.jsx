86a1ca64ca79b07b7870aabdaffb492fbf79615f
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2022-06-05 20:18:31 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Auth, Blogs, Blog } from "./components";
+
+function App() {
+  return (
+    <div>
+      {/* <Auth /> */}
+      <div>
+        <Blogs />
+      </div>
+      Content Management System
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Blogs.jsx
@@ -0,0 +1,247 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div class="py-16 bg-gradient-to-br h-screen from-yellow-50 to-red-100">
+      <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div class="mb-12 space-y-2 text-center">
+          <span class="block w-max mx-auto px-3 py-1.5 border border-yellow-200 rounded-full bg-yellow-100 text-yellow-700">
+            Blogs
+          </span>
+          <h2 class="text-2xl text-yellow-900 font-bold md:text-4xl">
+            A few of the top blogs
+          </h2>
+          <p class="lg:w-6/12 lg:mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+            pariatur at quam debitis repellat qui asperiores, animi ab fugiat
+            est.
+          </p>
+        </div>
+
+        <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          <div class="relative p-1 pb-16 rounded-xl group bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+            <div class="relative">
+              <div
+                aria-hidden="true"
+                class="absolute inset-x-0 bottom-0 h-64 bg-gradient-to-b from-transparent to-yellow-50"
+              ></div>
+              <img
+                src="../../assets/images/woman.jpg"
+                alt="art cover"
+                loading="lazy"
+                width="1000"
+                height="667"
+                class="h-64 w-full object-cover rounded-t-lg transition duration-500 group-hover:rounded-t-xl"
+              />
+            </div>
+            <div class="relative space-y-4 -mt-20 p-4">
+              <h4 class="text-2xl font-semibold text-yellow-900">
+                De fuga fugiat lorem ispum laboriosam expedita.
+              </h4>
+              <p class="text-gray-600">
+                Voluptates harum aliquam totam, doloribus eum impedit atque!
+                Temporibus...
+              </p>
+            </div>
+            <a
+              href="#"
+              class="absolute inset-x-4 bottom-4 flex items-center space-x-2"
+            >
+              <div
+                aria-hidden="true"
+                class="h-10 w-10 flex border border-yellow-200 rounded-full"
+              >
+                <svg
+                  aria-hidden="true"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  class="my-auto mx-auto flex-none text-yellow-700"
+                >
+                  <path
+                    d="M10.75 8.75l3.5 3.25-3.5 3.25"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </div>
+              <span class="text-yellow-600">Read more</span>
+            </a>
+          </div>
+          <div class="relative p-1 pb-16 rounded-xl group bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+            <div class="relative">
+              <div
+                aria-hidden="true"
+                class="absolute inset-x-0 bottom-0 h-64 bg-gradient-to-b from-transparent to-yellow-50"
+              ></div>
+              <img
+                src="../../assets/images/man.jpg"
+                alt="art cover"
+                loading="lazy"
+                width="1000"
+                height="667"
+                class="h-64 w-full object-cover object-top rounded-t-lg transition duration-500 group-hover:rounded-t-xl"
+              />
+            </div>
+            <div class="relative space-y-4 -mt-20 p-4">
+              <h4 class="text-2xl font-semibold text-yellow-900">
+                lorem ispum laboriosam expedita.
+              </h4>
+              <p class="text-gray-600">
+                Voluptates harum aliquam totam, doloribus eum impedit atque!
+                Temporibus...
+              </p>
+            </div>
+            <a
+              href="#"
+              class="absolute inset-x-4 bottom-4 flex items-center space-x-2"
+            >
+              <div
+                aria-hidden="true"
+                class="h-10 w-10 flex border border-yellow-200 rounded-full"
+              >
+                <svg
+                  aria-hidden="true"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  class="my-auto mx-auto flex-none text-yellow-700"
+                >
+                  <path
+                    d="M10.75 8.75l3.5 3.25-3.5 3.25"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </div>
+              <span class="text-yellow-600">Read more</span>
+            </a>
+          </div>
+          <div class="relative p-1 pb-16 rounded-xl group bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+            <div class="relative">
+              <div
+                aria-hidden="true"
+                class="absolute inset-x-0 bottom-0 h-64 bg-gradient-to-b from-transparent to-yellow-50"
+              ></div>
+              <img
+                src="../../assets/images/kid.jpg"
+                alt="art cover"
+                loading="lazy"
+                width="1000"
+                height="667"
+                class="h-64 w-full object-cover object-top rounded-t-lg transition duration-500 group-hover:rounded-t-xl"
+              />
+            </div>
+            <div class="relative space-y-4 -mt-20 p-4">
+              <h4 class="text-2xl font-semibold text-yellow-900">
+                De fuga fugiat lorem ispum laboriosam expedita.
+              </h4>
+              <p class="text-gray-600">
+                Voluptates harum aliquam totam, doloribus eum impedit atque!
+                Temporibus...
+              </p>
+            </div>
+            <a
+              href="#"
+              class="absolute inset-x-4 bottom-4 flex items-center space-x-2"
+            >
+              <div
+                aria-hidden="true"
+                class="h-10 w-10 flex border border-yellow-200 rounded-full"
+              >
+                <svg
+                  aria-hidden="true"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  class="my-auto mx-auto flex-none text-yellow-700"
+                >
+                  <path
+                    d="M10.75 8.75l3.5 3.25-3.5 3.25"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </div>
+              <span class="text-yellow-600">Read more</span>
+            </a>
+          </div>
+          <div class="relative p-1 pb-16 rounded-xl group bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+            <div class="relative">
+              <div
+                aria-hidden="true"
+                class="absolute inset-x-0 bottom-0 h-64 bg-gradient-to-b from-transparent to-yellow-50"
+              ></div>
+              <img
+                src="../../assets/images/woman.jpg"
+                alt="art cover"
+                loading="lazy"
+                width="1000"
+                height="667"
+                class="h-64 w-full object-cover rounded-t-lg transition duration-500 group-hover:rounded-t-xl"
+              />
+            </div>
+            <div class="relative space-y-4 -mt-20 p-4">
+              <h4 class="text-2xl font-semibold text-yellow-900">
+                lorem ispum laboriosam expedita.
+              </h4>
+              <p class="text-gray-600">
+                Voluptates harum aliquam totam, doloribus eum impedit atque!
+                Temporibus...
+              </p>
+            </div>
+            <a
+              href="#"
+              class="absolute inset-x-4 bottom-4 flex items-center space-x-2"
+            >
+              <div
+                aria-hidden="true"
+                class="h-10 w-10 flex border border-yellow-200 rounded-full"
+              >
+                <svg
+                  aria-hidden="true"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  class="my-auto mx-auto flex-none text-yellow-700"
+                >
+                  <path
+                    d="M10.75 8.75l3.5 3.25-3.5 3.25"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </div>
+              <span class="text-yellow-600">Read more</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-10 mb-20">
+        <button className="flex flex-1 items-start m-auto align-middle font-bold group hover:text-slate-500">
+          <svg
+            width="34"
+            height="34"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z" />
+          </svg>
+          {" Add new"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
